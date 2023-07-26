e092c98702b9afef829beb89b48c13979ea93b4a
vessoutraore <yvtraore94@yahoo.com>
2023-07-26 10:52:59 +0000

    Tasks 3. The last digit

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,30 +1,15 @@
 # !/usr/bin/python3
-# import random
-# number = random.randint(-10, 10)
-# 
-# Check if the number is greater than 0, equal to 0, or less than 0, and print the corresponding message
-# if number > 0:
-    # print("{} is positive.".format(number))
-# elif number == 0:
-    # print("0 is zero.")
-# else:
-    # print("{} is negative.".format(number))
-
 import random
 number = random.randint(-10, 10)
-# YOUR CODE HERE
-a = "is positive"
-b = "is negative"
-c = "is zero"
-if number>0: 
-    print(number, a, sep=" ")
-if number<0: 
-    print(number, b, sep=" ")
-if number==0: 
-    print(number, c, sep=" ")
 
-
-    
+# Check if the number is greater than 0, equal to 0, or less than 0, and print the corresponding message
+if number > 0:
+    print("{} is positive.".format(number))
+elif number == 0:
+    print("0 is zero.")
+else:
+    print("{} is negative.".format(number))
+    # 
 # !/usr/bin/python3
 # import random
 # number = random.randint(-10, 10)
--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+Last = abs(number) % 10
+
+if number < 0:
+    Last = -Last
+
+print("Last digit of", number, "is", Last, end=" ")
+
+if Last > 5:
+    print("and is greater than 5")
+elif Last == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
+
+
+#!/usr/bin/python3
 # import random
 # number = random.randint(-10000, 10000)
 # YOUR CODE HERE

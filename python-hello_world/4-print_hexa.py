e5a90ec20fd90dfcf0fc4ecf7f3ad1eedc3d2c53
vessoutraore <yvtraore94@yahoo.com>
2023-07-26 12:09:40 +0000

    Tasks - 4. Hexadecimal printing

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,5 +1,5 @@
-for i in range(0,99):
-     print(i, "= 0x""{:0x}" .format(i), end="\n")
-     
-# for num in range(99):
-    # print("{:2d} = 0x{:x}".format(num, num))
+# for i in range(0,99):
+    #  print(i, "= 0x""{:0x}" .format(i), end="\n")
+
+for num in range(99):
+    print("{:2d} = 0x{:x}".format(num, num), end="\n")
